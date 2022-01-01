78a63ac5032a7bb4ddd51b03daf8edc23717d30c
SubhamGit <subhamgw@users.github.com>
2022-01-01 16:08:19 +0530

    Calculator
    A material designed online calculator.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+function myFunctioni() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+
+function myFunctionToggle(x) {
+  x.classList.toggle("fa-toggle-on");
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function myFunctionn() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
